32399e5ac4f5787bb6289b9c02749d2416396fb2
photran25633@gmail.com <photran25633@gmail.com>
2023-07-12 22:56:16 +0700

    web agrios

--- src/pages/AgriOS/TableListTransfer/index.tsx
+++ src/pages/AgriOS/TableListTransfer/index.tsx
@@ -1,6 +1,6 @@
 import { customAPIAdd, customAPIGet } from '@/services/ant-design-pro/api';
 import { ExclamationCircleOutlined, ReloadOutlined, SearchOutlined } from '@ant-design/icons';
-import { ActionType, ModalForm, ProColumns, ProFormDigit, ProFormSelect, ProFormText } from '@ant-design/pro-components';
+import { ActionType, ModalForm, ProColumns, ProFormDigit, ProFormSelect } from '@ant-design/pro-components';
 import {
   ProTable,
 } from '@ant-design/pro-components';
@@ -174,23 +174,21 @@ const TableListAssignCPass = () => {
     {
       key: 'code',
       dataIndex: 'code',
-      // title: <FormattedMessage id='pages.se archTable.column.cPass' defaultMessage=' Aleger(AlegerID, SĐT, Email, CCCD/Hộ chiếu)' />,
       title: configDefaultText['page.transfer.column.aleger'],
       ...getColumnSearchProps('id'),
       render: (_, entity: any) => {
         return (
           <>
-            {entity?.fullname ? entity?.fullname : entity?.username}-{entity?.id}
+            <span style={{
+              color: entity?.blocked ? 'red' : ''
+            }}>{entity?.fullname ? entity?.fullname : entity?.username}-{entity?.id}</span>
             <br /> {entity?.phone}{entity?.phone && entity.email ? `|` : ''}{entity?.email}
             <br /> {entity?.passport ? `CCCD/HC:${entity?.passport}` : ``}
           </>
         );
       },
     },
-
-
     {
-      // title: <FormattedMessage id='pages.searchTable.column.ale' defaultMessage='Số dư Ale' />,
       title: configDefaultText['page.transfer.column.ale'],
       dataIndex: 'ale',
       valueType: 'textarea',
@@ -200,7 +198,6 @@ const TableListAssignCPass = () => {
       }
     },
     {
-      // title: <FormattedMessage id='pages.searchTable.column.availableBalance' defaultMessage='Số dư Ale khả dụng' />,
       title: configDefaultText['page.transfer.column.availableBalance'],
       dataIndex: 'availableBalance',
       valueType: 'textarea',
@@ -210,7 +207,6 @@ const TableListAssignCPass = () => {
       }
     },
     {
-      // title: <FormattedMessage id='pages.searchTable.column.produceAle' defaultMessage='Số dư ProduceAle' />,
       title: configDefaultText['page.transfer.column.produceAle'],
       dataIndex: 'produceAle',
       valueType: 'textarea',
@@ -221,7 +217,6 @@ const TableListAssignCPass = () => {
     },
 
     {
-      // title: <FormattedMessage id='pages.searchTable.column.promoAle' defaultMessage='Số dư PromoAle' />,
       title: configDefaultText['page.transfer.column.promoAle'],
       dataIndex: 'promoAle',
       valueType: 'textarea',
@@ -232,41 +227,51 @@ const TableListAssignCPass = () => {
     },
 
     {
-      // title: <FormattedMessage id='pages.searchTable.column.config' defaultMessage='Thao tác' />,
       title: configDefaultText['titleOption'],
       dataIndex: 'config',
       valueType: 'textarea',
       key: 'config',
       align: 'center',
       render: (_, text: any) => {
-        return [
-          <>
-            <Tooltip title={configDefaultText['page.transfer.transfer']}> <MdOutlineCompareArrows
-              style={{
-                fontSize: 20,
-              }}
-              onClick={() => {
-                setShowDialog(true);
-                setCurrentRowUser(text);
+        if (!text?.blocked) {
+          return <Space>
+              <Tooltip title={configDefaultText['page.transfer.transfer']}><Button
+                onClick={() => {
+                  setShowDialog(true);
+                  setCurrentRowUser(text);
+                }}
+                style={{
+                  marginLeft: '3px',
+                  padding: 0,
+                }}
+                key={1}
+                icon={<MdOutlineCompareArrows style={{
+                  fontSize: 20,
+                }} />}
+              />
+              </Tooltip>
+              <Tooltip title={configDefaultText['page.transfer.transferProduceAle']}><Button
+              key={2}
+                style={{
+                  marginLeft: '3px',
+                  padding: 0,
+                }}
+                icon={<MdCurrencyExchange />}
+                onClick={() => {
+                  setShowModal(true);
+                  setCurrentRowUser(text);
+                }}
+              />
+              </Tooltip>
+            </Space>
+          
+        }
+        else {
+          return <span style={{
+            color: 'red'
+          }}>Bị khóa</span>
+        }
 
-              }}
-            /></Tooltip>
-          </>,
-          <>
-            <Tooltip title={configDefaultText['page.transfer.transferProduceAle']}> <MdCurrencyExchange
-              style={{
-                fontSize: 20,
-                paddingLeft: 5,
-                // color: '#66FFFF'
-              }}
-              onClick={() => {
-                setShowModal(true);
-                setCurrentRowUser(text);
-              }}
-
-            /></Tooltip>
-          </>
-        ]
       }
     },
   ];
@@ -306,7 +311,7 @@ const TableListAssignCPass = () => {
         }}
 
         columns={columns}
-      
+
 
         pagination={{
 
@@ -354,7 +359,7 @@ const TableListAssignCPass = () => {
               confirm({
                 senderId: currentRowUser?.id,
                 produceAle: convertAle
-              }, `Aleger ${currentRowUser.fullname ? currentRowUser.fullname : currentRowUser.username} - ${currentRowUser.id}: Chắc chắn chuyển ${convertAle} ProduceAle sang  ${convertAle * rateConvert?.ratePromo}  Ale không?`, 'transactions/transfer-promo-admin');
+              }, `Aleger ${currentRowUser.fullname ? currentRowUser.fullname : currentRowUser.username} - ${currentRowUser.id}: Chắc chắn chuyển ${convertAle} ProduceAle sang  ${(convertAle * rateConvert?.ratePromo).toFixed(2)}  Ale không?`, 'transactions/transfer-promo-admin');
             }
             else {
               /// transfer produceAle to PromoAle
@@ -372,8 +377,6 @@ const TableListAssignCPass = () => {
 
         submitter={{
           searchConfig: {
-            // resetText: <FormattedMessage id='buttonClose' defaultMessage='Đóng' />,
-            // submitText: <FormattedMessage id='buttonSubmit' defaultMessage='Xác nhận' />,
             resetText: configDefaultText['buttonClose'],
             submitText: configDefaultText['buttonUpdate'],
           },
@@ -446,13 +449,14 @@ const TableListAssignCPass = () => {
             <>
               <Row gutter={24} className="m-0">
                 <Col span={24} className="gutter-row p-0" >
-                  <ProFormText
+                  <ProFormDigit
                     placeholder={configDefaultText['page.transfer.rePromoAle']}
                     label={configDefaultText['page.transfer.rePromoAle']}
                     name="promoAle"
                     disabled
                     fieldProps={{
                       value: convertAle * rateConvert?.ratePromo,
+                      precision: 2
                     }}
                   />
                 </Col>
@@ -461,13 +465,14 @@ const TableListAssignCPass = () => {
           ) : (
             <Row gutter={24} className="m-0">
               <Col span={24} className="gutter-row p-0" >
-                <ProFormText
+                <ProFormDigit
                   placeholder={configDefaultText['page.transfer.reAle']}
                   label={configDefaultText['page.transfer.reAle']}
                   name="ale"
                   disabled
                   fieldProps={{
                     value: convertAle,
+                    precision: 2
                   }}
                 />   </Col>
             </Row>
